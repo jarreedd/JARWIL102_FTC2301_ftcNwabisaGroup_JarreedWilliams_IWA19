5901c70b68a2ac29ee0a028b4ba91a12245ce62c
jarreed <jarreedw@gmail.com>
2023-05-06 08:57:30 +0200

    added more books to page

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ let page = 1;
 // }
 
 const fragment = document.createDocumentFragment()
-const extracted = books.slice(0, 36)
+let extracted = books.slice(0, BOOKS_PER_PAGE)
 
 const createPreview = (props) => {
     const {author, id, image, title} = props
@@ -82,25 +82,55 @@ document.querySelector("[data-list-items]").appendChild(fragment);
 
 // documentElement.style.setProperty('--color-dark', css[v].dark);
 // documentElement.style.setProperty('--color-light', css[v].light);
-// data-list-button = "Show more (books.length - BOOKS_PER_PAGE)"
 
-// data-list-button.disabled = !(matches.length - [page * BOOKS_PER_PAGE] > 0)
+const showmoreButton = document.querySelector('[data-list-button]')
 
-// data-list-button.innerHTML = /* html */ [
-//     '<span>Show more</span>',
-//     '<span class="list__remaining"> (${matches.length - [page * BOOKS_PER_PAGE] > 0 ? matches.length - [page * BOOKS_PER_PAGE] : 0})</span>',
-// ]
+const updateRemaining = () => {
+    const remaining = books.length - (BOOKS_PER_PAGE * page)
+    return remaining;
+}
+
+showmoreButton.innerHTML = /* html */ `
+    Show more 
+    <span class="list__remaining">
+        (${updateRemaining()})
+    </span>
+`;
 
 // data-search-cancel.click() { data-search-overlay.open === false }
 // data-settings-cancel.click() { querySelect(data-settings-overlay).open === false }
 // data-settings-form.submit() { actions.settings.submit }
 // data-list-close.click() { data-list-active.open === false }
 
-// data-list-button.click() {
-//     document.querySelector([data-list-items]).appendChild(createPreviewsFragment(matches, page x BOOKS_PER_PAGE, {page + 1} x BOOKS_PER_PAGE]))
-//     actions.list.updateRemaining()
-//     page = page + 1
-// }
+const showMore = (event) => {
+    event.preventDefault()
+    page += 1
+    const remaining = updateRemaining()
+    const hasRemaining = remaining > 0 ? remaining : 0
+
+    const rangeStart = (page - 1) * BOOKS_PER_PAGE
+    const rangeEnd = books.length - remaining
+
+    extracted = books.slice(rangeStart, rangeEnd)
+
+    if (hasRemaining > 0) {
+        for (const booksIndex of extracted) {
+            const preview = createPreview(booksIndex)
+            fragment.appendChild(preview)
+        }
+        
+        document.querySelector("[data-list-items]").appendChild(fragment);
+    }
+    
+    showmoreButton.innerHTML = /* html */ `
+    Show more 
+    <span class="list__remaining">
+        (${hasRemaining})
+    </span>
+    `; 
+};
+
+showmoreButton.addEventListener("click", showMore) 
 
 // data-header-search.click() {
 //     data-search-overlay.open === true ;
@@ -129,43 +159,6 @@ document.querySelector("[data-list-items]").appendChild(fragment);
 //     data-list-message.class.add('list__message_show')
 //     else data-list-message.class.remove('list__message_show')
     
-
-//     data-list-items.innerHTML = ''
-//     const fragment = document.createDocumentFragment()
-//     const extracted = source.slice(range[0], range[1])
-
-//     for ({ author, image, title, id }; extracted; i++) {
-//         const { author: authorId, id, image, title } = props
-
-//         element = document.createElement('button')
-//         element.classList = 'preview'
-//         element.setAttribute('data-preview', id)
-
-//         element.innerHTML = /* html */ `
-//             <img
-//                 class="preview__image"
-//                 src="${image}"
-//             />
-            
-//             <div class="preview__info">
-//                 <h3 class="preview__title">${title}</h3>
-//                 <div class="preview__author">${authors[authorId]}</div>
-//             </div>
-//         `
-
-//         fragment.appendChild(element)
-//     }
-    
-//     data-list-items.appendChild(fragments)
-//     initial === matches.length - [page * BOOKS_PER_PAGE]
-//     remaining === hasRemaining ? initial : 0
-//     data-list-button.disabled = initial > 0
-
-//     data-list-button.innerHTML = /* html */ `
-//         <span>Show more</span>
-//         <span class="list__remaining"> (${remaining})</span>
-//     `
-
 //     window.scrollTo({ top: 0, behavior: 'smooth' });
 //     data-search-overlay.open = false
 // }
